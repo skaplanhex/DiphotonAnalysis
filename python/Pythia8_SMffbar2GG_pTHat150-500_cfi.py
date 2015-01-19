4f03892fb781d3175ecd2a7432bf49f90959efe4
Steven Kaplan <skaplan@physics.rutgers.edu>
2015-01-19 13:23:28 -0600

    steve mrenna plot script, cfis for pythia8 SM diphoton events

---
+++ python/Pythia8_SMffbar2GG_pTHat150-500_cfi.py
@@ -0,0 +1,27 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_10_1_lHw.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_11_1_P0b.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_12_1_K1Y.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_13_1_6MN.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_14_1_fPM.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_15_1_UKv.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_16_1_qxb.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_17_1_h6I.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_18_1_Uky.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_19_1_raR.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_1_1_vGw.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_20_1_oRO.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_2_1_ryO.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_3_1_nJA.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_4_1_CYI.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_5_1_sxj.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_6_1_3MJ.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_7_1_X56.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_8_1_KsS.root',
+        '/store/user/skaplan/noreplica/pythia8_SMffbar2GG/pTHat150-500/SMffbar2GG_GEN_9_1_q8N.root',
+    )
+
+)
--- python/createCfis.py
+++ python/createCfis.py
@@ -1,25 +1,28 @@
 import os
 
-for lt in ("100K","2000","3000","3500","4000","4500","5000","6000"):
-    for p in ("150-500","500-Inf"):
-        folder="LambdaT%s_pTHat%s/"%(lt,p)
-        eospath = "/eos/uscms/store/user/skaplan/noreplica/LambdaTStudy/"
-        shortpath = "/store/user/skaplan/noreplica/LambdaTStudy/"
-        fullpath = eospath+folder
-        outfiles = os.listdir(fullpath)
-        outfilesnew=[]
-        for f in outfiles:
-            outfilesnew.append("        '"+shortpath + folder + f + "',")
-        fname = "ADDGravitontoGG_M1200_LambdaT%s_pTHat%s_cfi.py"%(lt,p)
-        print "Creating %s"%fname
-        out = open(fname,'w')
-        out.write("import FWCore.ParameterSet.Config as cms\n")
-        out.write("\n")
-        out.write('source = cms.Source("PoolSource",\n')
-        out.write("    fileNames = cms.untracked.vstring(\n")
-        for f in outfilesnew:
-            out.write(f+"\n")
-        out.write("    )\n")
-        out.write("\n")
-        out.write(")\n")
-        out.close()
+# for lt in ("100K","2000","3000","3500","4000","4500","5000","6000"):
+for p in ("150-500","500-Inf"):
+    # folder="LambdaT%s_pTHat%s/"%(lt,p)
+    folder = "pTHat%s/"%p
+    # eospath = "/eos/uscms/store/user/skaplan/noreplica/LambdaTStudy/"
+    eospath = '/eos/uscms/store/user/skaplan/noreplica/pythia8_SMffbar2GG/'
+    shortpath = "/store/user/skaplan/noreplica/pythia8_SMffbar2GG/"
+    fullpath = eospath+folder
+    outfiles = os.listdir(fullpath)
+    outfilesnew=[]
+    for f in outfiles:
+        outfilesnew.append("        '"+shortpath + folder + f + "',")
+    # fname = "ADDGravitontoGG_M1200_LambdaT%s_pTHat%s_cfi.py"%(lt,p)
+    fname = "Pythia8_SMffbar2GG_pTHat%s_cfi.py"%p
+    print "Creating %s"%fname
+    out = open(fname,'w')
+    out.write("import FWCore.ParameterSet.Config as cms\n")
+    out.write("\n")
+    out.write('source = cms.Source("PoolSource",\n')
+    out.write("    fileNames = cms.untracked.vstring(\n")
+    for f in outfilesnew:
+        out.write(f+"\n")
+    out.write("    )\n")
+    out.write("\n")
+    out.write(")\n")
+    out.close()
