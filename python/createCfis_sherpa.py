a9831a95e73da0e23e01cf041aa6fdd459fb5240
Steven Kaplan <skaplan@physics.rutgers.edu>
2015-09-03 14:36:21 -0500

    change cfis for more stats in sherpa

--- python/createCfis_sherpa.py
+++ python/createCfis_sherpa.py
@@ -1,20 +1,30 @@
 import os
 
+userDict = {
+    "skaplan" : "sherpa",
+    "hn99" : "sherpa_morestats"
+}
+
 for lt in (2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,100000):
     for p in ("200-750","750-2000","2000-ms"):
-            # if lt == 2000 and p == "500-Inf":
-            #     continue
-        # folder="LambdaT%i_mHat%s/"%(lt,p)
+
         folder = "mgg%s_Ms%i/"%(p,lt)
-        # folder = "pTHat%s/"%p
-        # eospath = "/eos/uscms/store/user/skaplan/noreplica/LambdaTStudy/"
-        eospath = '/eos/uscms/store/user/skaplan/noreplica/ADDdiPhoton/sherpa/'
-        shortpath = "/store/user/skaplan/noreplica/ADDdiPhoton/sherpa/"
-        fullpath = eospath+folder
-        outfiles = os.listdir(fullpath)
+            
+        eospathsk = '/eos/uscms/store/user/skaplan/noreplica/ADDdiPhoton/sherpa/'
+        shortpathsk = "/store/user/skaplan/noreplica/ADDdiPhoton/sherpa/"
+        fullpathsk = eospathsk+folder
+
+        eospathharan = '/eos/uscms/store/user/hn99/noreplica/ADDdiPhoton/sherpa_morestats/'
+        shortpathharan = "/store/user/hn99/noreplica/ADDdiPhoton/sherpa_morestats/"
+        fullpathharan = eospathharan+folder
+        outfilessk = os.listdir(fullpathsk)
+        outfilesharan = os.listdir(fullpathharan)
+        print len(outfilessk),len(outfilesharan)
         outfilesnew=[]
-        for f in outfiles:
-            outfilesnew.append("        '"+shortpath + folder + f + "',")
+        for f in outfilessk:
+            outfilesnew.append("        '"+shortpathsk + folder + f + "',")
+        for f in outfilesharan:
+            outfilesnew.append("        '"+shortpathharan + folder + f + "',")
         # fname = "ADDGravitontoGG_M1200_LambdaT%s_pTHat%s_cfi.py"%(lt,p)
         fname = "ADDdiPhoton_sherpa_13TeV_KK1_NED4_MS%i_MGG%s_cfi.py"%(lt,p)
         # fname = "ADDdiPhoton_LambdaT%i_pTHat%s_Hewett1_cfi.py"%(lt,p)
--- submitJobs.py
+++ submitJobs.py
@@ -5,10 +5,11 @@ import os
 
 # jdls = glob.glob("*.jdl")
 # only run Ms=2500 GeV jobs
-jdls = glob.glob("*ADDdiPhoton*.jdl")
+# jdls = glob.glob("*ADDdiPhoton*.jdl")
 # jdls = glob.glob("*6500*.jdl") + glob.glob("*7000*.jdl")
 # jdls = glob.glob("*Neg*jdl")
-
+# jdls = glob.glob("ADDdiPhoton_sherpa_13TeV_KK1_NED4*jdl")
+jdls = ("ADDdiPhoton_sherpa_13TeV_KK1_NED4_MS2500_MGG2000-ms.jdl","ADDdiPhoton_sherpa_13TeV_KK1_NED4_MS3000_MGG200-750.jdl","ADDdiPhoton_sherpa_13TeV_KK1_NED4_MS4000_MGG2000-ms.jdl","ADDdiPhoton_sherpa_13TeV_KK1_NED4_MS7000_MGG2000-ms.jdl")
 for jdl in jdls:
     if jdl == "condor.jdl":
         continue
